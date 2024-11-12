6fecc3dd55b3fe7ba4a75e3afc31bc35b839bd2b
Mateo <mateo.avalos3942@gmail.com>
2024-11-12 07:22:41 -0500

    Added .sh script for answer 12.c

---
+++ backup-s3.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Define el directorio y el bucket
+storage_dir="/home/mateo/OperatingSystems-Final/backup"
+bucket_name="mae-so-ueia-2024"
+
+# Sube archivos JSON al bucket y luego los elimina
+for file in "$storage_dir"/*.json; do
+    if [ -f "$file" ]; then
+        aws s3 cp "$file" s3://$bucket_name/
+        rm "$file"
+    fi
+done
